81a37ed4f53d0c8149758b867a25dae919df78ab
rosen1000 <rosen.ruseff@gmail.com>
2025-03-29 01:56:54 +0200

    add router

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,17 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './pages/App.tsx';
 import { BrowserRouter, Route, Routes } from 'react-router';
+import Login from './pages/auth/Login.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>
+	<StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route index element={<App />} />
+				<Route path='auth'>
+					<Route path='login' element={<Login />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	</StrictMode>
 );
---
+++ src/pages/auth/Login.tsx
@@ -0,0 +1,4 @@
+export default function Login() {
+	// TODO: Placeholder
+	return <div>Login</div>;
+}
