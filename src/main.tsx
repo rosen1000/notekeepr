e6c7cc2bb2baab3ace2f211c4d361dc772178b2b
rosen1000 <rosen.ruseff@gmail.com>
2025-03-30 05:48:48 +0300

    add material ui

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,24 @@ import './index.css';
 import App from './pages/App.tsx';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import Login from './pages/auth/Login.tsx';
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+
+const theme = createTheme({
+	palette: { mode: 'dark', },
+});
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<BrowserRouter>
-			<Routes>
-				<Route index element={<App />} />
-				<Route path='auth'>
-					<Route path='login' element={<Login />} />
-				</Route>
-			</Routes>
-		</BrowserRouter>
-	</StrictMode>,
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<BrowserRouter>
+				<Routes>
+					<Route index element={<App />} />
+					<Route path='auth'>
+						<Route path='login' element={<Login />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</ThemeProvider>
+	</StrictMode>
 );
