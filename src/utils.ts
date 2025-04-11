43e80d111f3bb6ce1103879d9f6139001e138240
rosen1000 <rosen.ruseff@gmail.com>
2025-04-11 19:17:24 +0300

    feat: set utility for setting part of stateful object

--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,12 @@
-export const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*.-]).{6,}$/
+import { Dispatch, SetStateAction } from 'react';
+
+export const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*.-]).{6,}$/;
 export const passwordLength = 6;
+
+export function set<T extends Record<string, any>, K extends keyof T, V extends T[K]>(
+	setter: Dispatch<SetStateAction<T>>,
+	key: K,
+	value: V
+) {
+	setter((curr) => ({ ...curr, [key]: value }));
+}
