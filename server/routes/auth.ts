b329fd92eb7f6a81383932575b315d188bb02f8b
rosen1000 <rosen.ruseff@gmail.com>
2025-04-22 11:14:39 +0300

    feat: add users id to jwt token

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -3,22 +3,20 @@ import db from '../database';
 import { app as main } from '..';
 import { z } from 'zod';
 import { JwtPayload } from '../@types';
+import hasher from '../hasher';
 
-// TODO: use hashid for user ids
 export default (app: typeof main) => {
 	app.post(
 		'/login',
 		{ schema: { body: z.object({ username: z.string(), password: z.string() }) } },
 		async (req, res) => {
-			console.log(req.body);
-			const user = await db.user.findUnique({
-				where: { username: req.body.username },
-			});
+			const user = await db.user.findUnique({ where: { username: req.body.username } });
 			if (!user || !argon2.verify(user.password, req.body.password)) {
 				return res.status(401).send('Invalid credentials');
 			}
 
-			const token = app.jwt.sign({ username: user.username });
+			const id = hasher.encode(user.id);
+			const token = app.jwt.sign({ id, username: user.username });
 			res.setCookie('Authorization', token, { httpOnly: true, secure: true, sameSite: 'none', path: '/' });
 			return res.status(200).send();
 		}
@@ -26,30 +24,21 @@ export default (app: typeof main) => {
 
 	app.post(
 		'/register',
-		{
-			schema: {
-				body: z.object({
-					username: z.string(),
-					password: z.string(),
-				}),
-			},
-		},
+		{ schema: { body: z.object({ username: z.string(), password: z.string() }) } },
 		async (req, res) => {
-			const user = await db.user.findUnique({
-				where: { username: req.body.username },
-			});
-			if (user) {
-				return res.status(409).send('Username already exists');
-			}
+			let user = await db.user.findUnique({ where: { username: req.body.username } });
+			if (user) return res.status(409).send('Username already exists');
+
 			const hashedPassword = await argon2.hash(req.body.password);
-			await db.user.create({
+			user = await db.user.create({
 				data: {
 					username: req.body.username,
 					password: hashedPassword,
 				},
 			});
 
-			const token = app.jwt.sign({ username: req.body.username });
+			const id = hasher.encode(user.id);
+			const token = app.jwt.sign({ id, username: req.body.username });
 			res.setCookie('Authorization', token, { httpOnly: true, secure: true, sameSite: 'none', path: '/' });
 			return res.status(201).send();
 		}
@@ -59,9 +48,7 @@ export default (app: typeof main) => {
 		try {
 			const jwt = (await req.jwtVerify()) as JwtPayload;
 			const user = db.user.findFirst({
-				where: {
-					username: jwt.username,
-				},
+				where: { username: jwt.username },
 				omit: { password: true },
 			});
 			return user;
